65feef786b555f05557766fe3689e8cb99f1e171
Preet Patel <preet.patel2@ontariotechu.net>
2023-03-09 13:18:36 -0500

    Adding Final Submission Design Task Files

---
+++ Project Data Storage and Kafka connects/Final/publisher.py
@@ -0,0 +1,50 @@
+from google.cloud import pubsub_v1
+import os
+import time
+import numpy as np
+import random 
+import json
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="<Credential File Path>"
+project_id = "<Project-id>"
+topic_id = "smartmeter_in"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+#device normal distributions profile used to generate random data
+DEVICE_PROFILES = {
+	"boston": {'temp': (51.3, 17.7), 'humd': (77.4, 18.7), 'pres': (1.019, 0.091) },
+	"denver": {'temp': (49.5, 19.3), 'humd': (33.0, 13.9), 'pres': (1.512, 0.341) },
+	"losang": {'temp': (63.9, 11.7), 'humd': (62.8, 21.8), 'pres': (1.215, 0.201) },
+}
+
+profileNames=["boston","denver","losang"];
+
+for n in range(1, 10):
+    profile_name = profileNames[random.randint(0, 2)];
+    profile = DEVICE_PROFILES[profile_name]
+    # get random values within a normal distribution of the value
+    temp = max(0, np.random.normal(profile['temp'][0], profile['temp'][1]))
+    humd = max(0, min(np.random.normal(profile['humd'][0], profile['humd'][1]), 100))
+    pres = max(0, np.random.normal(profile['pres'][0], profile['pres'][1]))
+    
+    # create dictionary
+    msg={"time": time.time(), "profile_name": profile_name, "temperature": temp,"humidity": humd, "pressure":pres};
+    
+    #randomly eliminate some measurements
+    for i in range(3):
+        if(random.randrange(0,10)<1):
+            choice=random.randrange(0,3)
+            if(choice==0):
+                msg['temperature']=None;
+            elif (choice==1):
+                msg['humidity']=None;
+            else:
+                msg['pressure']=None;
+    print(msg)
+    # data_str = f"{msg}"
+    future = publisher.publish(topic_path, json.dumps(msg).encode('utf-8'));
+    print(future.result())
+
+print(f"Published messages to {topic_path}.")
---
+++ Project Data Storage and Kafka connects/Final/subscriber.py
@@ -0,0 +1,21 @@
+import os
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="<Credential File Path>"
+project_id = "<Project-id>"
+subscription_id = "smartmeter_out-sub"
+
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    print(f"Received message.")
+    print(f"{message.data}\n")
+    message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+with subscriber:
+        streaming_pull_future.result()  # Block until the shutdown is complete.
